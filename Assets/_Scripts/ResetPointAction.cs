4011b9be2ce52587dd4ce1782f87d9108e4cca90
DannyAlgorithmic <38365455+DannyAlgorithmic@users.noreply.github.com>
2018-08-15 02:07:46 -0400

    Temperary Fix For Stack Overflow issue
    It doesn't reset the point tile for now, it just reloads the scene.

--- Assets/_Scripts/ResetPointAction.cs
+++ Assets/_Scripts/ResetPointAction.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 [CreateAssetMenu (menuName = "Actions/Reset Point Tile")]
 public class ResetPointAction : BaseAction {
     public override void TakeAction(PlayerController ctrl)
     {
-        GlobalMap.SetPointTile();
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+        // GlobalMap.SetPointTile();
     }
 }
